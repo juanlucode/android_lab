6b8b734a6d0bc4fb0155f3c2b6c41ca4d41eac63
juanluisga <juanluis.garciarodriguez@gmail.com>
2017-01-22 10:03:57 +0100

    Service Center

--- ServiceCenter/app/src/main/java/com/example/juanlucode/servicecenter/MainActivity.java
+++ ServiceCenter/app/src/main/java/com/example/juanlucode/servicecenter/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.juanlucode.servicecenter;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnNavegador;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNavegador = (Button) this.findViewById(R.id.btnNavegador);
+        btnNavegador.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnNavegador:
+                intent = new Intent(this, NavegadorActivity.class);
+                this.startActivity(intent);
+                break;
+        }
     }
 }
---
+++ ServiceCenter/app/src/main/java/com/example/juanlucode/servicecenter/NavegadorActivity.java
@@ -0,0 +1,13 @@
+package com.example.juanlucode.servicecenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NavegadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navegador);
+    }
+}
