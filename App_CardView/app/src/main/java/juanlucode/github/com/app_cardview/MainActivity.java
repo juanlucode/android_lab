97d17bfd60d004893790b32b8f387ae4c8499016
juanluisga <juanluis.garciarodriguez@gmail.com>
2017-02-12 09:46:59 +0100

    varios

--- App_CardView/app/src/main/java/juanlucode/github/com/app_cardview/MainActivity.java
+++ App_CardView/app/src/main/java/juanlucode/github/com/app_cardview/MainActivity.java
@@ -2,12 +2,19 @@ package juanlucode.github.com.app_cardview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textoTextView = (TextView) findViewById(R.id.textoTextView);
+
+        textoTextView.setText("Hola, sr. programador...");
     }
 }
