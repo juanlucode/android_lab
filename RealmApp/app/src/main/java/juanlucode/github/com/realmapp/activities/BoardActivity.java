8e3f253ab2d5a4525f1ae9b41b9fc726d51b0990
juanluisga <juanluis.garciarodriguez@gmail.com>
2017-02-18 10:25:53 +0100

    RealmApp Alert Dialog

--- RealmApp/app/src/main/java/juanlucode/github/com/realmapp/activities/BoardActivity.java
+++ RealmApp/app/src/main/java/juanlucode/github/com/realmapp/activities/BoardActivity.java
@@ -1,7 +1,13 @@
 package juanlucode.github.com.realmapp.activities;
 
+import android.content.DialogInterface;
+import android.support.annotation.StyleRes;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import juanlucode.github.com.realmapp.R;
 
@@ -11,5 +17,38 @@ public class BoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.board_main);
+        showAddBoardDialog();
+
+
+    }
+
+    private void showAddBoardDialog(){
+        
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        final View viewDialog = (View) getLayoutInflater().inflate(R.layout.add_board_dialog, null);
+        final EditText titleEditText = (EditText) viewDialog.findViewById(R.id.titleEditText);
+        builder.setView(viewDialog);
+        builder.setTitle("Adding new board...");
+        builder.setMessage("Enter the new board's title:");
+
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String titleBoard = titleEditText.getText().toString().trim();
+                if ( titleBoard.isEmpty() ){
+                    Toast.makeText( getApplicationContext(), 
+                                    "Title is required.", 
+                                    Toast.LENGTH_SHORT).show();
+                } else {
+                    addBoard();
+                }
+            }
+        });
+
+        builder.create().show();;
+
+    }
+
+    private void addBoard() {
     }
 }
